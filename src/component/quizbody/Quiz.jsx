d8325e739d754ca941ca1b71cf6dd21e61b0b1bc
AshishRaj0602 <115613437+AshishRaj0602@users.noreply.github.com>
2023-03-26 01:01:46 +0530

    commitabc

--- src/component/quizbody/Quiz.jsx
+++ src/component/quizbody/Quiz.jsx
@@ -1,10 +1,19 @@
 import React,{useState,useEffect}from 'react'
+import useSound from 'use-sound';
+// import play from '../Assert/waw.mp3';
+import correct from '../Assert/correct2.mp3';
+import wrong from '../Assert/wron.mp3';
 import "./quiz.css"
 const Quiz = ({data,setStop,queNum,setqueNum}) => { 
   const [question, setQuestion] =useState(null);
   const [selectedAns, setSelectedAns] =useState(null);
   const [className, setClassName] =useState("answer");
-
+  // const [letsPlay] =useSound(play);
+  const [correctAnswer] =useSound(correct);
+  const [wrongAnswer] =useSound(wrong);
+  //  useEffect(()=>{
+  //     letsPlay(); 
+  //    },[letsPlay])
   const delay = (duration,callback)=>{
     setTimeout(()=>{
       callback();
@@ -16,17 +25,23 @@ const Quiz = ({data,setStop,queNum,setqueNum}) => {
     delay(0,()=>setClassName(opt.correct ? "answer correct" : "answer wrong"));
     delay(3000,()=>{
       if(opt.correct){
+        correctAnswer();
         setqueNum((prev)=> prev+1);
         setSelectedAns(null);
       }
       else{
         setStop(true);
+        wrongAnswer();
       }
     });
     // setTimeout(()=>{
     //   setClassName(opt.correct ? "answer correct" : "answer wrong");
     //    },3000);
      }
+
+    //  useEffect(()=>{
+    //   letsPlay(); 
+    //  },[letsPlay])
   useEffect(() => {
     setQuestion(data[queNum-1]);
   }, [data,queNum]);
